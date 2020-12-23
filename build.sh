e29ceb3300e8303a101f0a360f41d7447e4b9952
snimmagadda1 <saiguy@me.com>
2020-12-23 13:05:46 -0600

    Update CICD

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@
 mvn clean package
 
 echo "Spring batch build completed"
+echo "Build & push"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build . -t snimmagadda/stacke-batch-mysql:"$TRAVIS_BUILD_NUMBER"
+docker tag snimmagadda/spring-batch-rapid-starter:"$TRAVIS_BUILD_NUMBER" snimmagadda/spring-batch-rapid-starter:latest
+docker push snimmagadda/spring-batch-rapid-starter:"$TRAVIS_BUILD_NUMBER" && docker push snimmagadda/spring-batch-rapid-starter:latest
