030314a248c642371a9f832218753b3148ee05fd
Sai Nimmagadda <saiguy@me.com>
2021-06-06 01:38:59 -0400

    Revert "Spelling, flow"
    This reverts commit 3e7690f5488ef47405a1c023b09a6d597d6b1a3f.

--- src/main/java/com/batch/config/batch/BatchConfig.java
+++ src/main/java/com/batch/config/batch/BatchConfig.java
@@ -4,16 +4,12 @@ import com.batch.config.readers.ReadersConfig;
 import com.batch.config.writers.WritersConfig;
 import com.batch.model.Person;
 import com.batch.processor.PersonItemProcessor;
-
-import org.hibernate.InvalidMappingException;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
@@ -51,15 +47,11 @@ public class BatchConfig {
      * @return Job - configured job
      */
     @Bean
-    public Job coolBatchJob() {
+    public Job importUserJob() {
         return jobBuilderFactory
-                .get("coolBatchJob")
-                .start(stepA())
-                .on("FAILED")
-                    .to(failure())
-                .from(stepA())
-                .on("COMPLETED")
-                    .to(flow1())
+                .get("importUserJob")
+                .incrementer(new RunIdIncrementer())
+                .flow(step1())
                 .end()
                 .build();
     }
@@ -71,48 +63,11 @@ public class BatchConfig {
     @Bean
     public Step step1() {
         return stepBuilderFactory
-                .get("step1Real")
+                .get("step1")
                 .<Person, Person>chunk(10)
                 .reader(readers.readerCsv())
                 .processor(processor())
                 .writer(writers.csvItemWriter())
                 .build();
     }
-
-    @Bean
-    public Step flow1() {
-        return stepBuilderFactory.get("step1").tasklet((contribution, chunkContext) -> {
-            System.out.println("Running the success flow!!");
-            return RepeatStatus.FINISHED;
-        }).build();
-    }
-
-    @Bean
-    public Step failure() {
-        return stepBuilderFactory.get("flow2").tasklet((contribution, chunkContext) -> {
-            System.out.println("Running the failure flow!!");
-            return RepeatStatus.FINISHED;
-        }).build();
-    }
-
-    @Bean
-    public Step stepA() {
-        return stepBuilderFactory.get("stepA").tasklet((contribution, chunkContext) -> {
-            System.out.println("Woo");
-            return RepeatStatus.FINISHED;
-        }).build();
-    }
- 
-
-    @Bean
-    public Step stepB() {
-        return stepBuilderFactory.get("stepB").tasklet((contribution, chunkContext) -> {
-            System.out.println("Hoo!");
-            if (true) {
-                throw new InvalidMappingException("Failed", "test", "/test");
-            }
-            return RepeatStatus.FINISHED;
-        }).build();
-    }
- 
 }
