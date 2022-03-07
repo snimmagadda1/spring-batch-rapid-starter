df28967138aac772070899fb661d6f2785fa96b2
Sai Nimmagadda <saiguy@Sais-Mac-mini.attlocal.net>
2022-03-06 19:14:32 -0500

    Better naming of datasource

--- src/main/java/com/batch/config/db/SpringDatasourceConfiguration.java
+++ src/main/java/com/batch/config/db/SpringDatasourceConfiguration.java
@@ -1,5 +1,8 @@
 package com.batch.config.db;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,47 +12,42 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringDatasourceConfiguration {
 
-    @Bean(name = "springDataSource")
-    @Primary
-    public DataSource springDataSource(
-            SpringDatasourceConfigurationProperties springDatasourceConfigurationProperties) {
-        DriverManagerDataSource datasource = new DriverManagerDataSource();
-        datasource.setDriverClassName(springDatasourceConfigurationProperties.getDriverClassName());
-        datasource.setUrl(springDatasourceConfigurationProperties.getUrl());
-        datasource.setUsername(springDatasourceConfigurationProperties.getUsername());
-        datasource.setPassword(springDatasourceConfigurationProperties.getPassword());
-        return datasource;
-    }
+  @Bean(name = "springDataSource")
+  @Primary
+  public DataSource springDataSource(
+      SpringDatasourceConfigurationProperties springDatasourceConfigurationProperties) {
+    DriverManagerDataSource datasource = new DriverManagerDataSource();
+    datasource.setDriverClassName(springDatasourceConfigurationProperties.getDriverClassName());
+    datasource.setUrl(springDatasourceConfigurationProperties.getUrl());
+    datasource.setUsername(springDatasourceConfigurationProperties.getUsername());
+    datasource.setPassword(springDatasourceConfigurationProperties.getPassword());
+    return datasource;
+  }
 
-    @Primary
-    @Bean(name = "springEntityManager")
-    @PersistenceContext(unitName = "springPersU")
-    public LocalContainerEntityManagerFactoryBean appEntityManagerFactory(
-            @Qualifier("springDataSource") DataSource dataSource) {
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setGenerateDdl(true);
-        LocalContainerEntityManagerFactoryBean factory =
-                new LocalContainerEntityManagerFactoryBean();
-        factory.setDataSource(dataSource);
-        factory.setPackagesToScan("com.snimma1.model");
-        factory.setPersistenceUnitName("springPersU");
-        factory.setJpaVendorAdapter(adapter);
-        return factory;
-    }
+  @Primary
+  @Bean(name = "springEntityManager")
+  @PersistenceContext(unitName = "springPersU")
+  public LocalContainerEntityManagerFactoryBean springEntityManagerFactory(
+      @Qualifier("springDataSource") DataSource dataSource) {
+    HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+    adapter.setGenerateDdl(true);
+    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+    factory.setDataSource(dataSource);
+    factory.setPackagesToScan("com.snimma1.model");
+    factory.setPersistenceUnitName("springPersU");
+    factory.setJpaVendorAdapter(adapter);
+    return factory;
+  }
 
-    @Primary
-    @Bean
-    public JpaTransactionManager springJpaTransactionManager(
-            @Qualifier("springEntityManager") EntityManagerFactory factory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(factory);
-        return transactionManager;
-    }
+  @Primary
+  @Bean
+  public JpaTransactionManager springJpaTransactionManager(
+      @Qualifier("springEntityManager") EntityManagerFactory factory) {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(factory);
+    return transactionManager;
+  }
 }
